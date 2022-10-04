078c219647fb5f03873cbafcfb71a75ef8a00950
Mateusz Wajnberger <mateusz.wajnberger@gmail.com>
2022-10-04 20:23:29 +0200

    add menu style

--- Core/View.php
+++ Core/View.php
@@ -8,7 +8,7 @@ class View
     public static function render($view, $args = []){
         extract($args, EXTR_SKIP);
 
-        $file = dirname(__DIR__) . "/App/Views/$view";  
+        $file = "../App/Views/$view";  
 
         if (is_readable($file)) {
             require $file;
@@ -19,14 +19,19 @@ class View
 
     public static function renderTemplate($template, $args = [])
     {
+        echo static::getTemplate($template, $args);
+    }
+
+    public static function getTemplate($template, $args = [])
+    {
         static $twig = null;
 
         if ($twig === null) {
-            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $loader = new \Twig\Loader\FilesystemLoader('../App/Views');
             $twig = new \Twig\Environment($loader);
-            $twig->addGlobal('current_user',\App\Auth::getUser());
+            $twig->addGlobal('current_user', \App\Auth::getUser());
         }
 
-        echo $twig->render($template, $args);
+        return $twig->render($template, $args);
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,18 +2,19 @@
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-//$loader = new \Twig\Loader\ArrayLoader();
-//$twig = new \Twig\Environment($loader);
 
 error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
+
+session_start();
 
 $router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('signup', ['controller' => 'Signup', 'action' => 'new']);
+$router->add('signup/activate/{token:[\da-f]+}', ['controller' => 'Signup', 'action' => 'activate']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
 $router->add('menu', ['controller' => 'Menu', 'action' => 'main']);
 $router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
