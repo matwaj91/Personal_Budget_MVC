f41c599d2552f685256b00ff8c878881705f3413
Mateusz Wajnberger <mateusz.wajnberger@gmail.com>
2022-11-04 21:46:06 +0100

    add change account view

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -5,14 +5,23 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Auth;
 use \App\Flash;
+use \App\Models\Settings;
+use \App\Models\Setchanges;
 
-class Profile extends Authenticated{
+class Profile extends \Core\Controller{
 
     protected function before(){
 
         parent::before();
 
         $this->user = Auth::getUser();
+    }
+
+    public function changeAccountAction(){
+
+        View::renderTemplate('Menu/user.twig', [
+            'user' => $this->user
+        ]);
     }
 
     public function editAction(){
@@ -37,4 +46,35 @@ class Profile extends Authenticated{
             ]);
         }
     }
+
+    public function deleteTransactionsAction(){
+
+		$deleteTransaction = new Setchanges();
+
+		if($deleteTransaction->deleteAllTransactions()){
+
+			Flash::addMessage("All transactions have been deleted");
+			$this->redirect('/Menu/main');
+		}
+		else{
+            
+			Flash::addMessage("An unknown error occurred. Please try again", Flash::WARNING);
+			$this->redirect('/Menu/main');
+		}
+	}
+
+    /*public function deleteAccountAction(){
+
+		if(Setchanges::deleteAccount()){		
+
+			Auth::logout();
+			Flash::addMessage("Konto zostało usunięte");
+			$this->redirect('/');
+		}
+		else{
+
+			Flash::addMessage("Wystąpił błąd, spróbuj ponownie", Flash::WARNING);
+			$this->redirect('/');
+		}
+	}*/
 }
